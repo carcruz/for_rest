2ad1fc3455dc0a9d32dc7b505bbb96b63d439b61
Carlos Cruz <me@carcruzcast.com>
2023-09-01 13:18:48 +0200

    feat: add text

--- web/src/sections/FactorsOfGrowing/FactorsOfGrowing.jsx
+++ web/src/sections/FactorsOfGrowing/FactorsOfGrowing.jsx
@@ -11,12 +11,30 @@ function FactorsOfGrowing() {
             <div className="w-1/2">
               <h3>Gradual addition of complexity</h3>
               <div style={{ height: "450px" }} className="pt-12">
-                hi
+                <p>
+                  Distribution of trees in a forest is not random, since local
+                  competition powered by the laws of metabolic growth determines
+                  population size distribution. Proximity can be a limiting
+                  factor, so can be access to water.
+                </p>
+                <br />
+                <p>
+                  This behaviour can be modelled. Lee et al, PNAS 2021 have
+                  developed a minimal dynamical model that integrates timescales
+                  of individual growth and mortality with competitive attrition
+                  on a background of fluctuating resources
+                </p>
+                <br />
+                <p>
+                  Our minimal quantitative theory unifies spatiotemporal
+                  patterns across sessile organisms through local competition
+                  mediated by the laws of metabolic growth, which in turn, are
+                  the result of long-term evolutionary dynamics.
+                </p>
               </div>
             </div>
             <div className="w-1/2">
-              <h3>Resources</h3>
-              <div style={{ height: "450px" }} className="pt-12">
+              <div className="">
                 <Resources />
               </div>
             </div>
