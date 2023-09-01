b826671f95c9f66cd8f04f11378f4da775339b27
William Schueller <william.schueller@gmail.com>
2023-09-01 12:10:19 +0200

    map data

---
+++ web/src/sections/Model/data/listing.js
@@ -0,0 +1,33 @@
+
+import { panamamodelMapData, panamarealMapData } from "./data/panama/mapData";
+
+const panamaMapState = {
+  name: "Barro Colorado Island, Panama",
+  realData: panamarealMapData,
+  modelData: panamamodelMapData,
+};
+	
+import { turkenschanzparkmodelMapData, turkenschanzparkrealMapData } from "./data/turkenschanzpark/mapData";
+
+const turkenschanzparkMapState = {
+  name: "Türkenschanzpark, Vienna, Austria",
+  realData: turkenschanzparkrealMapData,
+  modelData: turkenschanzparkmodelMapData,
+};
+	
+import { josefstadtmodelMapData, josefstadtrealMapData } from "./data/josefstadt/mapData";
+
+const josefstadtMapState = {
+  name: "Josefstadt, Vienna, Austria",
+  realData: josefstadtrealMapData,
+  modelData: josefstadtmodelMapData,
+};
+	
+import { alaskamodelMapData, alaskarealMapData } from "./data/alaska/mapData";
+
+const alaskaMapState = {
+  name: "Alaska",
+  realData: alaskarealMapData,
+  modelData: alaskamodelMapData,
+};
+	
